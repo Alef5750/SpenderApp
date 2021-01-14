378a0edd80899c57b418243f8762a10e56eda6db
Roi-Livne <roilivne14@gmail.com>
2021-01-14 07:26:38 +0200

    server setup

---
+++ spender-backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const cors = require('cors')
+
+// set cors
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+const bodyParser = require("body-parser");
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+//Routes
+
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
+
+// handle termination on ctrl+c to close mongo connection
+process.on('SIGINT', () => {
+  console.info('SIGTERM signal received.');
+  mongoClient.close(false, () => {
+    console.log('MongoDb connection closed.');
+    process.exit(0);
+  });
+});
