e5df29b34c917574c75c251e721de3520fbf8b3a
alef <alewitt@gmail.com>
2021-01-14 15:24:01 +0200

    downloaded react-router-dom

--- spender-frontend/src/components/navbar.jsx
+++ spender-frontend/src/components/navbar.jsx
@@ -1,34 +1,27 @@
-import { BrowserRouter as Router, Route, Switch, Link, } from "react-router-dom"
-import Charts from "./charts"
-import HomePage from "./home-page"
-import Settings from "./settings"
-import Money from "./money"
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import HomePage from "../pages/HomePage";
 
 function Navbar() {
-    
-    return (
-    
-        <Router>
-            <div className="mt-5">
-                <Navbar
-                    fixed="top"
-                    className="bg-primary mx-5 rounded d-flex justify-content-around"
-                >
-                    <Link to="/">Home</Link>
-                    <Link to="/money">$</Link>
-                    <Link to="/charts">Charts</Link>
-                    <Link to="/settings">Settings</Link>
-                </Navbar>
-                <Switch>
-                    <Route exact path="/"><HomePage /></Route>
-                    <Route path="/money"><Money /></Route>
-                    <Route path="/charts"><Charts /></Route>
-                    <Route path="/settings"><Settings /></Route>
-                </Switch>
-            </div>
-        </Router>
-    )
-
+  return (
+    <Router>
+      <div className="mt-5">
+        <Navbar
+          fixed="top"
+          className="bg-primary mx-5 rounded d-flex justify-content-around"
+        >
+          <Link to="/">Home</Link>
+          <Link to="/money">$</Link>
+          <Link to="/charts">Charts</Link>
+          <Link to="/settings">Settings</Link>
+        </Navbar>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default Navbar
+export default Navbar;
