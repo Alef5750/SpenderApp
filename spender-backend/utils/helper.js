ee3f2b1494e0021ba6e4bfc011dd47ff8559832b
Roi-Livne <roilivne14@gmail.com>
2021-01-16 11:38:50 +0200

    added helper func to insert new expense in appropriate year/month

--- spender-backend/utils/helper.js
+++ spender-backend/utils/helper.js
@@ -5,4 +5,18 @@ const isEmpty = (obj) => {
   }
   return true;
 }
+
+const insertExpense = (newExpense, userExpenses) => {
+  const years = Object.keys(userExpenses);
+  const lastYear = years[years.length - 1];
+  const months = Object.keys(userExpenses[lastYear])
+  const lastMonth = months[months.length - 1];
+
+  const currentMonthExpenses = userExpenses[lastYear][lastMonth]
+  const newMonthExpenses = [...currentMonthExpenses, newExpense]
+
+  userExpenses[lastYear][lastMonth] = newMonthExpenses;
+
+  return userExpenses;
+}
 module.exports = { isEmpty }
