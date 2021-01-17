dab9d16aaa01340af7384f9102411e46f55721d1
Ram Smithson <ramsmithson1@gmail.com>
2021-01-17 10:43:30 +0200

    merging navbar

--- spender-frontend/src/App.js
+++ spender-frontend/src/App.js
@@ -3,8 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Router from "./Router";
 import Navigation from "./components/Navigation";
 
-// I added the navigation to the App, that way, you can navigate throughout the app
-
 function App() {
   return (
     <div className="App">
