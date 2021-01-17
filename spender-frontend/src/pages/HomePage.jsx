4d3a94c0acddfa5665122f0626f896d7abdb1a6f
Ram Smithson <ramsmithson1@gmail.com>
2021-01-17 19:46:09 +0200

    turning the button to links connected to coresponding pages

--- spender-frontend/src/pages/HomePage.jsx
+++ spender-frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Footer from "../components/footer"
 import Navigation from "../components/Navigation";
 import $ from "../images/$ icon.png"
@@ -13,20 +13,31 @@ export default function HomePage() {
       <Navigation />
         <h1>Hey User</h1>
         <div className="mx-4 vh-100">
-          <Button className="my-4 py-4 col bg-danger border-white d-flex align-items-center justify-content-around">
+        
+          <Link
+            className="btn btn-danger my-4 py-4 col bg-danger d-flex align-items-center justify-content-around"
+            to="/expenses"
+          >
             <img src={$} alt={"$"}/>
             <span className="h3 mb-0">New Expense</span>
-          </Button>
+          </Link>
         
-          <Button className="my-4 py-4 col bg-success border-white d-flex align-items-center justify-content-around">
+          <Link
+            className="btn btn-success my-4 py-4 col bg-success d-flex align-items-center justify-content-around"
+            to="/charts"
+          >
             <img src={chartsIcon} alt={"Charts"} />
             <span className="h3 mb-0">View Reports</span>
-          </Button>
-        
-          <Button className="my-4 py-4 col bg-primary border-white d-flex align-items-center justify-content-start">
+          </Link>
+
+          <Link
+            className="btn btn-primary my-4 py-4 col bg-primary d-flex align-items-center justify-content-start"
+            to="/settings"
+          >
             <img className="mx-4" src={settingsIcon} alt={"Settings"} />
             <span className="h3 mb-0 ml-3">Settings</span>
-          </Button>
+          </Link>
+        
         </div>
         <Footer/>
     </div>
