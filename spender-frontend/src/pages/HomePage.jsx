af308895df70b71f5938dba2f3877e24ae98f1fa
alef <alewitt@gmail.com>
2021-01-17 20:24:40 +0200

    downsized images folder

--- spender-frontend/src/pages/HomePage.jsx
+++ spender-frontend/src/pages/HomePage.jsx
@@ -1,45 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Footer from "../components/footer"
+import Footer from "../components/footer";
 import Navigation from "../components/Navigation";
-import $ from "../images/$ icon.png"
-import chartsIcon from "../images/charts icon.png"
-import settingsIcon from "../images/settings Icon.png"
-
+import $ from "../images/ExpensesIconWhite.png";
+import chartsIcon from "../images/ReportsIconWhite.png";
+import settingsIcon from "../images/SettingsIconWhite.png";
 
 export default function HomePage() {
   return (
     <div>
       <Navigation />
-        <h1>Hey User</h1>
-        <div className="mx-4 vh-100">
-        
-          <Link
-            className="btn btn-danger my-4 py-4 col bg-danger d-flex align-items-center justify-content-around"
-            to="/expenses"
-          >
-            <img src={$} alt={"$"}/>
-            <span className="h3 mb-0">New Expense</span>
-          </Link>
-        
-          <Link
-            className="btn btn-success my-4 py-4 col bg-success d-flex align-items-center justify-content-around"
-            to="/charts"
-          >
-            <img src={chartsIcon} alt={"Charts"} />
-            <span className="h3 mb-0">View Reports</span>
-          </Link>
+      <h1>Hey User</h1>
+      <div className="mx-4 vh-100">
+        <Link
+          className="btn btn-danger my-4 py-4 col bg-danger d-flex align-items-center justify-content-around"
+          to="/expenses"
+        >
+          <img src={$} alt={"$"} />
+          <span className="h3 mb-0">New Expense</span>
+        </Link>
 
-          <Link
-            className="btn btn-primary my-4 py-4 col bg-primary d-flex align-items-center justify-content-start"
-            to="/settings"
-          >
-            <img className="mx-4" src={settingsIcon} alt={"Settings"} />
-            <span className="h3 mb-0 ml-3">Settings</span>
-          </Link>
-        
-        </div>
-        <Footer/>
+        <Link
+          className="btn btn-success my-4 py-4 col bg-success d-flex align-items-center justify-content-around"
+          to="/charts"
+        >
+          <img src={chartsIcon} alt={"Charts"} />
+          <span className="h3 mb-0">View Reports</span>
+        </Link>
+
+        <Link
+          className="btn btn-primary my-4 py-4 col bg-primary d-flex align-items-center justify-content-start"
+          to="/settings"
+        >
+          <img className="mx-4" src={settingsIcon} alt={"Settings"} />
+          <span className="h3 mb-0 ml-3">Settings</span>
+        </Link>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
