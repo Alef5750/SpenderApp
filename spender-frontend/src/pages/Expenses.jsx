b7873841271e622b511337303990564d153a51f4
Roi-Livne <roilivne14@gmail.com>
2021-01-18 20:02:29 +0200

    added private routes, google sign in and user auth

--- spender-frontend/src/pages/Expenses.jsx
+++ spender-frontend/src/pages/Expenses.jsx
@@ -3,9 +3,9 @@ import Navigation from "../components/Navigation";
 
 export default function Expenses() {
     return (
-      <div>
-        <Navigation/>
-        <h1>Expenses Page</h1>;
-      </div>
+        <div>
+            <Navigation />
+            <h1>Expenses Page</h1>;
+        </div>
     );
 }
