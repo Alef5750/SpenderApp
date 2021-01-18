6d180692723e7aa3cf5e33c769226a478cb48be1
alef <alewitt@gmail.com>
2021-01-18 20:25:27 +0200

    added Formik form to NewExpense page

--- spender-frontend/src/pages/NewExpense.jsx
+++ spender-frontend/src/pages/NewExpense.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
 import { useLocation } from "react-router-dom";
+//styles
+import styles from "../styles/NewExpense.module.css";
+import { Button, Form, Alert } from "react-bootstrap";
+
+//validation
+const formSchema = Yup.object().shape({
+  title: Yup.string().required("Oops! You haven't entered a title"),
+  amount: Yup.string().required("Oops! You haven't entered an amount"),
+  description: Yup.string(),
+});
 
 export default function NewExpense() {
   const location = useLocation();
@@ -14,9 +27,60 @@ export default function NewExpense() {
     category = "Entertainment";
   else if (location.pathname === "/expenses/monthly")
     category = "Monthly Expenses";
+  function handleNewExpense(expense) {
+    console.log(expense);
+  }
   return (
-    <div>
-      <h1>{category}</h1>
-    </div>
+    <Formik
+      initialValues={{
+        title: "",
+        amount: null,
+        description: "",
+      }}
+      validationSchema={formSchema}
+      onSubmit={(expense) => handleNewExpense(expense)}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        errors,
+        touched,
+      }) => {
+        return (
+          <>
+            <h1>{category}</h1>
+            <Form className={styles.body} onSubmit={handleSubmit}>
+              <input
+                className={styles.input}
+                type="text"
+                name={"title"}
+                value={values.title}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.title && touched.title && (
+                <Alert variant="danger">{errors.title}</Alert>
+              )}
+              <input
+                className={styles.input}
+                type="number"
+                name={"amount"}
+                value={values.amount}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.amount && touched.amount && (
+                <Alert variant="danger">{errors.amount}</Alert>
+              )}
+              <Button className={styles.saveButton} type="submit">
+                Save
+              </Button>
+            </Form>
+          </>
+        );
+      }}
+    </Formik>
   );
 }
