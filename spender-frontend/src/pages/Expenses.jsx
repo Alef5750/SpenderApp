f8e7ed73d89ef39aa2a4f11a396c53c09beb9e5b
alef <alewitt@gmail.com>
2021-01-18 16:05:16 +0200

    moved styles to css module; added alt to images

--- spender-frontend/src/pages/Expenses.jsx
+++ spender-frontend/src/pages/Expenses.jsx
@@ -6,91 +6,91 @@ import expenses_food from "../images/expenses_food.png";
 import expenses_entertainement from "../images/expenses_entertainement.png";
 import expenses_other from "../images/expenses_other.png";
 import expenses_add_new from "../images/expenses_add_new.png";
-import "./Expenses.css";
+import styles from "../styles/Expenses.module.css";
 
 export default function Expenses() {
-
   const handleMonthly = () => {
     console.log("monthly");
-  }
+  };
   const handleFood = () => {
     console.log("food");
-  }
+  };
   const handleEntertainment = () => {
     console.log("entertainment");
-  }
+  };
   const handleOther = () => {
     console.log("other");
-  }
+  };
   const handleAddNew = () => {
     console.log("addnew");
-  }
+  };
   const handleEdit = () => {
     console.log("edit");
-  }
+  };
 
   return (
     <div>
       <Navigation />
 
       <div className="d-flex flex-row mx-4 px-1 mt-5 justify-content-between">
-        <div className=" d-flex flex-column expenses-first-features-div" onClick = {() => handleMonthly()}>
-          <div className= "h-50 py-4 ">
-          Monthly
-              
-          </div>
-          <div className= "h-50">
-              <img src = { expenses_monthly }/>
+        <div
+          className={`d-flex flex-column ${styles.expensesFirstFeaturesDiv}`}
+          onClick={() => handleMonthly()}
+        >
+          <div className="h-50 py-4 ">Monthly</div>
+          <div className="h-50">
+            <img src={expenses_monthly} alt="monthly" />
           </div>
         </div>
-        <div className=" d-flex flex-column expenses-first-features-div" onClick = {() => handleFood()}>
-        <div className= "h-50 py-4">
-          Food
-              
-          </div>
-          <div className= "h-50">
-              <img src = { expenses_food }/>
+        <div
+          className={`d-flex flex-column ${styles.expensesFirstFeaturesDiv}`}
+          onClick={() => handleFood()}
+        >
+          <div className="h-50 py-4">Food</div>
+          <div className="h-50">
+            <img src={expenses_food} alt="food" />
           </div>
         </div>
       </div>
 
       <div className="d-flex flex-row mx-4 px-1 mt-3 justify-content-between">
-        <div className=" d-flex flex-column expenses-first-features-div" onClick = {() => handleEntertainment()}>
-        <div className= "h-50 py-4">
-          Entertainment
-              
-          </div>
-          <div className= "h-50">
-              <img src = { expenses_entertainement }/>
+        <div
+          className={`d-flex flex-column ${styles.expensesFirstFeaturesDiv}`}
+          onClick={() => handleEntertainment()}
+        >
+          <div className="h-50 py-4">Entertainment</div>
+          <div className="h-50">
+            <img src={expenses_entertainement} alt="entertainment" />
           </div>
         </div>
-        <div className=" d-flex flex-column expenses-first-features-div" onClick = {() => handleOther()}>
-        <div className= "h-50 py-4">
-          Other
-              
-          </div>
-          <div className= "h-50">
-              <img src = { expenses_other }/>
+        <div
+          className={`d-flex flex-column ${styles.expensesFirstFeaturesDiv}`}
+          onClick={() => handleOther()}
+        >
+          <div className="h-50 py-4">Other</div>
+          <div className="h-50">
+            <img src={expenses_other} alt="other" />
           </div>
         </div>
       </div>
 
-      <div className="d-flex flex-row mx-4 px-1 mt-3 justify-content-start" >
-        <div className=" d-flex flex-column expenses-first-features-div" onClick = {() => handleAddNew()}>
-        <div className= "h-50 py-4 ">
-          Add New...
-          
-              
-          </div>
-          <div className= "h-50">
-              <img src = { expenses_add_new }/>
+      <div className="d-flex flex-row mx-4 px-1 mt-3 justify-content-start">
+        <div
+          className={`d-flex flex-column ${styles.expensesFirstFeaturesDiv}`}
+          onClick={() => handleAddNew()}
+        >
+          <div className="h-50 py-4 ">Add New...</div>
+          <div className="h-50">
+            <img src={expenses_add_new} alt="Add New" />
           </div>
         </div>
-        
       </div>
-    <Button className = "w-100 button-bottom"  onClick = {() => handleEdit()}>Edit</Button>
+      <Button
+        className={`w-100 ${styles.buttonBottom}`}
+        onClick={() => handleEdit()}
+      >
+        Edit
+      </Button>
     </div>
-    
   );
-  
 }
