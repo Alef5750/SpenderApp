b9ffd445d4a64bf2a8deb3fa422bbbea74552c05
alef <alewitt@gmail.com>
2021-01-19 13:32:36 +0200

    Router comments

--- spender-frontend/src/Router.jsx
+++ spender-frontend/src/Router.jsx
@@ -7,44 +7,44 @@ import Charts from "./pages/Charts";
 import PrivateRoute from "./components/PrivateRoute";
 
 export default function Router() {
-    return (
-        // <BrowserRouter>
-        //     <Switch>
-        //         <Route path="/charts">
-        //             <Charts />
-        //         </Route>
-        //         <Route path="/expenses">
-        //             <Expenses />
-        //         </Route>
-        //         <Route path="/settings">
-        //             <Settings />
-        //         </Route>
-        //         <Route path="/home">
-        //             <HomePage />
-        //         </Route>
-        //         <Route path="/">
-        //             <SignIn />
-        //         </Route>
-        //     </Switch>
-        // </BrowserRouter>
-        <BrowserRouter>
-            <Switch>
-                <PrivateRoute path="/charts" component={Charts}>
-                    {/* <Charts /> */}
-                </PrivateRoute>
-                <PrivateRoute path="/expenses" component={Expenses}>
-                    {/* <Expenses /> */}
-                </PrivateRoute>
-                <PrivateRoute path="/settings" component={Settings}>
-                    {/* <Settings /> */}
-                </PrivateRoute>
-                <PrivateRoute path="/home" component={HomePage}>
-                    {/* <HomePage /> */}
-                </PrivateRoute>
-                <PrivateRoute path="/" component={SignIn}>
-                    {/* <SignIn /> */}
-                </PrivateRoute>
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    // <BrowserRouter>
+    //     <Switch>
+    //         <Route path="/charts">
+    //             <Charts />
+    //         </Route>
+    //         <Route path="/expenses">
+    //             <Expenses />
+    //         </Route>
+    //         <Route path="/settings">
+    //             <Settings />
+    //         </Route>
+    //         <Route path="/home">
+    //             <HomePage />
+    //         </Route>
+    //         <Route path="/">
+    //             <SignIn />
+    //         </Route>
+    //     </Switch>
+    // </BrowserRouter>
+    <BrowserRouter>
+      <Switch>
+        <PrivateRoute path="/charts" component={Charts}>
+          {/* <Charts /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/expenses" component={Expenses}>
+          {/* <Expenses /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/settings" component={Settings}>
+          {/* <Settings /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/home" component={HomePage}>
+          {/* <HomePage /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/" component={SignIn}>
+          {/* <SignIn /> */}
+        </PrivateRoute>
+      </Switch>
+    </BrowserRouter>
+  );
 }
