2ec82a0aaf33b2c06c8fb10ca1475d865d3267f5
alef <alewitt@gmail.com>
2021-01-20 08:55:16 +0200

    attempted PUT request

--- spender-frontend/src/Router.jsx
+++ spender-frontend/src/Router.jsx
@@ -1,81 +1,81 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, /* Route */ } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import HomePage from "./pages/HomePage";
 import Expenses from "./pages/Expenses";
 import NewExpense from "./pages/NewExpense";
 import Settings from "./pages/Settings";
 import Charts from "./pages/Charts";
-// import PrivateRoute from "./components/PrivateRoute";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function Router() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/expenses/addnew">
-          <NewExpense />
-        </Route>{" "}
-        <Route path="/expenses/other">
-          <NewExpense />
-        </Route>{" "}
-        <Route path="/expenses/entertainment">
-          <NewExpense />
-        </Route>{" "}
-        <Route path="/expenses/food">
-          <NewExpense />
-        </Route>{" "}
-        <Route path="/expenses/monthly">
-          <Expenses />
-        </Route>
-        <Route path="/charts">
-          <Charts />
-        </Route>
-        <Route path="/expenses">
-          <Expenses />
-        </Route>
-        <Route path="/settings">
-          <Settings />
-        </Route>
-        <Route path="/home">
-          <HomePage />
-        </Route>
-        <Route path="/">
-          <SignIn />
-        </Route>
-      </Switch>
-    </BrowserRouter>
     // <BrowserRouter>
     //   <Switch>
-    //     <PrivateRoute path="/expenses/addnew" component={NewExpense}>
-    //       {/* <NewExpense /> */}
-    //     </PrivateRoute>
-    //     <PrivateRoute path="/expenses/other" component={NewExpense}>
-    //       {/* <NewExpense /><NewExpense /> */}
-    //     </PrivateRoute>
-    //     <PrivateRoute path="/expenses/entertainment" component={NewExpense}>
-    //       {/* <NewExpense /> */}
-    //     </PrivateRoute>
-    //     <PrivateRoute path="/expenses/food" component={NewExpense}>
-    //       {/* <NewExpense /> */}
-    //     </PrivateRoute>
-    //     <PrivateRoute path="/expenses/monthly" component={Expenses}>
-    //       {/* <Expenses /> */}
-    //     </PrivateRoute>
-    //     <PrivateRoute path="/charts" component={Charts}>
-    //       {/* <Charts /> */}
-    //     </PrivateRoute>
-    //     <PrivateRoute path="/expenses" component={Expenses}>
-    //       {/* <Expenses /> */}
-    //     </PrivateRoute>
-    //     <PrivateRoute path="/settings" component={Settings}>
-    //       {/* <Settings /> */}
-    //     </PrivateRoute>
-    //     <PrivateRoute path="/home" component={HomePage}>
-    //       {/* <HomePage /> */}
-    //     </PrivateRoute>
-    //     <PrivateRoute path="/" component={SignIn}>
-    //       {/* <SignIn /> */}
-    //     </PrivateRoute>
+    //     <Route path="/expenses/addnew">
+    //       <NewExpense />
+    //     </Route>{" "}
+    //     <Route path="/expenses/other">
+    //       <NewExpense />
+    //     </Route>{" "}
+    //     <Route path="/expenses/entertainment">
+    //       <NewExpense />
+    //     </Route>{" "}
+    //     <Route path="/expenses/food">
+    //       <NewExpense />
+    //     </Route>{" "}
+    //     <Route path="/expenses/monthly">
+    //       <Expenses />
+    //     </Route>
+    //     <Route path="/charts">
+    //       <Charts />
+    //     </Route>
+    //     <Route path="/expenses">
+    //       <Expenses />
+    //     </Route>
+    //     <Route path="/settings">
+    //       <Settings />
+    //     </Route>
+    //     <Route path="/home">
+    //       <HomePage />
+    //     </Route>
+    //     <Route path="/">
+    //       <SignIn />
+    //     </Route>
     //   </Switch>
-    // </BrowserRouter> 
+    // </BrowserRouter>
+    <BrowserRouter>
+      <Switch>
+        <PrivateRoute path="/expenses/addnew" component={NewExpense}>
+          {/* <NewExpense /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/expenses/other" component={NewExpense}>
+          {/* <NewExpense /><NewExpense /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/expenses/entertainment" component={NewExpense}>
+          {/* <NewExpense /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/expenses/food" component={NewExpense}>
+          {/* <NewExpense /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/expenses/monthly" component={Expenses}>
+          {/* <Expenses /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/charts" component={Charts}>
+          {/* <Charts /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/expenses" component={Expenses}>
+          {/* <Expenses /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/settings" component={Settings}>
+          {/* <Settings /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/home" component={HomePage}>
+          {/* <HomePage /> */}
+        </PrivateRoute>
+        <PrivateRoute path="/" component={SignIn}>
+          {/* <SignIn /> */}
+        </PrivateRoute>
+      </Switch>
+    </BrowserRouter> 
   );
 }
--- spender-frontend/src/helpers/api.js
+++ spender-frontend/src/helpers/api.js
@@ -1,9 +1,15 @@
 import axios from "axios";
+const backendURL = "http://localhost:5000";
 
 export function SaveNewExpense() {
   axios.put();
 }
 
-export function UpdateSettings() {
-  axios.put();
+export function UpdateSettings(settings) {
+  axios.put(`${backendURL}/api/user/:id`, JSON.stringify(settings));
+  console.log(
+    `We're sending ${JSON.stringify(
+      settings
+    )} as a PUT request at ${backendURL}/api/user/:id`
+  );
 }
