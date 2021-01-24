35c6b554cd1c0185c395c1b51d781a153f684e96
alef <alewitt@gmail.com>
2021-01-24 17:25:36 +0200

    created AddCategory.jsx page and added to Router.jsx

--- spender-frontend/src/Router.jsx
+++ spender-frontend/src/Router.jsx
@@ -6,28 +6,30 @@ import NewExpense from "./pages/NewExpense";
 import Settings from "./pages/Settings";
 import Charts from "./pages/Charts";
 import PrivateRoute from "./components/PrivateRoute";
+import AddCategory from "./pages/AddCategory";
 
 export default function Router() {
     return (
-        // <BrowserRouter>
-        //   <Switch>
-        //     <Route path="/expenses/*" />
-        //     <Route path="/charts" />
-        //     <Route path="/expenses" />
-        //     <Route path="/settings" />
-        //     <Route path="/home" />
-        //     <Route path="/" />
-        //   </Switch>
-        // </BrowserRouter>
-        <BrowserRouter>
-            <Switch>
-                <PrivateRoute path="/expenses/*" component={NewExpense} />
-                <PrivateRoute path="/charts" component={Charts} />
-                <PrivateRoute path="/expenses" component={Expenses} />
-                <PrivateRoute path="/settings" component={Settings} />
-                <PrivateRoute path="/home" component={HomePage} />
-                <PrivateRoute path="/" component={SignIn} />
-            </Switch>
-        </BrowserRouter>
+      // <BrowserRouter>
+      //   <Switch>
+      //     <Route path="/expenses/*" />
+      //     <Route path="/charts" />
+      //     <Route path="/expenses" />
+      //     <Route path="/settings" />
+      //     <Route path="/home" />
+      //     <Route path="/" />
+      //   </Switch>
+      // </BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <PrivateRoute path="/addcategory" component={AddCategory} />
+          <PrivateRoute path="/expenses/*" component={NewExpense} />
+          <PrivateRoute path="/charts" component={Charts} />
+          <PrivateRoute path="/expenses" component={Expenses} />
+          <PrivateRoute path="/settings" component={Settings} />
+          <PrivateRoute path="/home" component={HomePage} />
+          <PrivateRoute path="/" component={SignIn} />
+        </Switch>
+      </BrowserRouter>
     );
 }
