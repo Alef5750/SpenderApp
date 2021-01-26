7512e99891ad0422919f86dddf45a3f1ac034407
Roi-Livne <roilivne14@gmail.com>
2021-01-26 21:56:32 +0200

    cleaning page components

--- spender-frontend/src/pages/HomePage.jsx
+++ spender-frontend/src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-// import axios from "axios";
 
 //components etc.
 import Footer from "../components/footer";
@@ -13,52 +12,45 @@ import settingsIcon from "../images/SettingsIconWhite.png";
 //styles
 import styles from "../styles/HomePage.module.css";
 
-// const backendURL = "http://localhost:5000";
+export default function HomePage() {
+    const user = useContext(UserContext);
 
-export default function HomePage(props) {
-  const user = useContext(UserContext);
-  // const [displayName, setDisplayName] = useState("");
+    return (
+        <div>
+            <Navigation />
+            <h1 className={`${styles.text} ${styles.h1}`}>
+                Hey {user.displayName}
+            </h1>
+            <div className="mx-4 vh-100">
+                <Link
+                    className={`btn btn-danger my-4 py-4 col bg-danger d-flex align-items-center justify-content-around ${styles.buttons}`}
+                    to="/expenses"
+                >
+                    <img src={$} alt={"$"} />
+                    <span className="h3 mb-0">New Expense</span>
+                </Link>
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await axios.get(`${backendURL}/api/users/${userId}`);
-  //     console.log(result.data);
-  //     setDisplayName(result.data.displayName);
-  //   };
+                <Link
+                    className={`btn btn-danger my-4 py-4 col bg-success d-flex align-items-center justify-content-around ${styles.buttons}`}
+                    to="/charts"
+                >
+                    <img src={chartsIcon} alt={"Charts"} />
+                    <span className="h3 mb-0">View Reports</span>
+                </Link>
 
-  //   fetchData();
-  // }, [userId]);
-
-  return (
-    <div>
-      <Navigation />
-      <h1 className={`${styles.text} ${styles.h1}`}>Hey {user.displayName}</h1>
-      <div className="mx-4 vh-100">
-        <Link
-          className={`btn btn-danger my-4 py-4 col bg-danger d-flex align-items-center justify-content-around ${styles.buttons}`}
-          to="/expenses"
-        >
-          <img src={$} alt={"$"} />
-          <span className="h3 mb-0">New Expense</span>
-        </Link>
-
-        <Link
-          className={`btn btn-danger my-4 py-4 col bg-success d-flex align-items-center justify-content-around ${styles.buttons}`}
-          to="/charts"
-        >
-          <img src={chartsIcon} alt={"Charts"} />
-          <span className="h3 mb-0">View Reports</span>
-        </Link>
-
-        <Link
-          className={`btn btn-danger my-4 py-4 col bg-primary d-flex align-items-center ${styles.buttons}`}
-          to="/settings"
-        >
-          <img className="ml-3 mr-5" src={settingsIcon} alt={"Settings"} />
-          <span className="h3 mb-0 ml-0">Settings</span>
-        </Link>
-      </div>
-      <Footer />
-    </div>
-  );
+                <Link
+                    className={`btn btn-danger my-4 py-4 col bg-primary d-flex align-items-center ${styles.buttons}`}
+                    to="/settings"
+                >
+                    <img
+                        className="ml-3 mr-5"
+                        src={settingsIcon}
+                        alt={"Settings"}
+                    />
+                    <span className="h3 mb-0 ml-0">Settings</span>
+                </Link>
+            </div>
+            <Footer />
+        </div>
+    );
 }
--- spender-frontend/src/pages/NewExpense.jsx
+++ spender-frontend/src/pages/NewExpense.jsx
@@ -6,12 +6,9 @@ import { UserContext } from "../components/PrivateRoute";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import { useLocation, useHistory } from "react-router-dom";
-//helpers
-// import cards from "../helpers/categories";
 
 //componenets
 import Navigation from "../components/Navigation";
-// import { NewExpenseCategory } from "../helpers/conditionals";
 import { SaveNewExpense } from "../helpers/api";
 //styles
 import styles from "../styles/NewExpense.module.css";
@@ -19,111 +16,118 @@ import { Button, Form, Alert } from "react-bootstrap";
 
 //validation
 const formSchema = Yup.object().shape({
-  category: Yup.string().required("Please enter a category name").max(20),
-  title: Yup.string()
-    .required("Oops! You haven't entered a title")
-    .min(5)
-    .max(20),
-  amount: Yup.number().required("Oops! You haven't entered an amount"),
-  desc: Yup.string(),
+    category: Yup.string().required("Please enter a category name").max(20),
+    title: Yup.string()
+        .required("Oops! You haven't entered a title")
+        .min(5)
+        .max(20),
+    amount: Yup.number().required("Oops! You haven't entered an amount"),
+    desc: Yup.string(),
 });
 
 export default function NewExpense(props) {
-  const user = useContext(UserContext);
+    const user = useContext(UserContext);
 
-  const path = useLocation().pathname;
-  console.log(path);
-  const category = path.split("/")[2];
-  console.log(category);
-  let history  = useHistory();
+    const path = useLocation().pathname;
+    const category = path.split("/")[2];
+    let history = useHistory();
 
-  function handleNewExpense(expense) {
-    console.log(expense);
-    SaveNewExpense(expense, user._id); // used id /////////////////////////////////
-    history.push("/expenses");
-  }
-  return (
-    <Formik
-      initialValues={{
-        category: category,
-        title: "",
-        amount: "",
-        desc: "",
-      }}
-      validationSchema={formSchema}
-      onSubmit={(expense) => handleNewExpense(expense)}
-    >
-      {({
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        values,
-        errors,
-        touched,
-      }) => {
-        return (
-          <div className={styles.body}>
-            <Navigation />
-            <h1 className={`${styles.text} ${styles.h1}`}>{category}</h1>
-            <Form className={styles.form} onSubmit={handleSubmit}>
-              {errors.category && touched.category && (
-                <Alert variant="danger">{errors.category}</Alert>
-              )}
-              <Form.Group>
-                <h5 className={styles.text}>What was it?</h5>
-                <input
-                  className={styles.input}
-                  type="text"
-                  name={"title"}
-                  value={values.title}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-              </Form.Group>
-              {errors.title && touched.title && (
-                <Alert variant="danger">{errors.title}</Alert>
-              )}
-              <Form.Group>
-                <h5 className={styles.text}>How much did it cost?</h5>
-                <input
-                  className={styles.input}
-                  placeholder="$"
-                  type="number"
-                  name={"amount"}
-                  value={values.amount}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-              </Form.Group>
-              {errors.amount && touched.amount && (
-                <Alert variant="danger">{errors.amount}</Alert>
-              )}
-              <Form.Group>
-                <h5 className={styles.text}>Description (optional):</h5>
-                <textarea
-                  className={styles.textarea}
-                  cols="40"
-                  rows="4"
-                  type="text"
-                  name={"desc"}
-                  value={values.desc}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-              </Form.Group>
-              <Button className={styles.saveButton} type="submit">
-                Save
-              </Button>
-            </Form>
-            <Button
-              className={`w-100 ${styles.buttonBottom}`}
-              onClick={() => {history.push("/expenses")}}
-            >
-              Cancel
-            </Button>
-          </div>
-        );
-      }}
-    </Formik>
-  );
+    function handleNewExpense(expense) {
+        SaveNewExpense(expense, user._id);
+        history.push("/expenses");
+    }
+    return (
+        <Formik
+            initialValues={{
+                category: category,
+                title: "",
+                amount: "",
+                desc: "",
+            }}
+            validationSchema={formSchema}
+            onSubmit={(expense) => handleNewExpense(expense)}
+        >
+            {({
+                handleSubmit,
+                handleChange,
+                handleBlur,
+                values,
+                errors,
+                touched,
+            }) => {
+                return (
+                    <div className={styles.body}>
+                        <Navigation />
+                        <h1 className={`${styles.text} ${styles.h1}`}>
+                            {category}
+                        </h1>
+                        <Form className={styles.form} onSubmit={handleSubmit}>
+                            {errors.category && touched.category && (
+                                <Alert variant="danger">
+                                    {errors.category}
+                                </Alert>
+                            )}
+                            <Form.Group>
+                                <h5 className={styles.text}>What was it?</h5>
+                                <input
+                                    className={styles.input}
+                                    type="text"
+                                    name={"title"}
+                                    value={values.title}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                            </Form.Group>
+                            {errors.title && touched.title && (
+                                <Alert variant="danger">{errors.title}</Alert>
+                            )}
+                            <Form.Group>
+                                <h5 className={styles.text}>
+                                    How much did it cost?
+                                </h5>
+                                <input
+                                    className={styles.input}
+                                    placeholder="$"
+                                    type="number"
+                                    name={"amount"}
+                                    value={values.amount}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                            </Form.Group>
+                            {errors.amount && touched.amount && (
+                                <Alert variant="danger">{errors.amount}</Alert>
+                            )}
+                            <Form.Group>
+                                <h5 className={styles.text}>
+                                    Description (optional):
+                                </h5>
+                                <textarea
+                                    className={styles.textarea}
+                                    cols="40"
+                                    rows="4"
+                                    type="text"
+                                    name={"desc"}
+                                    value={values.desc}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                            </Form.Group>
+                            <Button className={styles.saveButton} type="submit">
+                                Save
+                            </Button>
+                        </Form>
+                        <Button
+                            className={`w-100 ${styles.buttonBottom}`}
+                            onClick={() => {
+                                history.push("/expenses");
+                            }}
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                );
+            }}
+        </Formik>
+    );
 }
