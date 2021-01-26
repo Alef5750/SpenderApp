f580867fcc772fc25bd8f59d91d14ebc0c7bd43a
Roi-Livne <roilivne14@gmail.com>
2021-01-26 22:05:41 +0200

    formatted and cleaned code in components

--- spender-frontend/src/components/Navigation.jsx
+++ spender-frontend/src/components/Navigation.jsx
@@ -1,33 +1,27 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-// import { getExpensesById } from "../helpers/api";
 
 //helpers
 import { Home, Expenses, Reports, Settings } from "../helpers/conditionals";
 //styles
 import styles from "../styles/Navbar.module.css";
 
-
 export default function Navigation() {
-
-  // getExpensesById("/api/users/600591c5a1e29824c0ef786a/expenses")
-  //   .then(response => console.log(response))
-  
-  return (
-    <Navbar className={styles.navBar} sticky="top" expand="sm">
-      <NavLink to="/home">
-        <img src={Home()} alt="Home" />
-      </NavLink>
-      <NavLink to="/expenses">
-        <img src={Expenses()} alt="Expenses" />
-      </NavLink>
-      <NavLink to="/charts">
-        <img src={Reports()} alt="Reports" />
-      </NavLink>
-      <NavLink to="/settings">
-        <img src={Settings()} alt="Settings" />
-      </NavLink>
-    </Navbar>
-  );
+    return (
+        <Navbar className={styles.navBar} sticky="top" expand="sm">
+            <NavLink to="/home">
+                <img src={Home()} alt="Home" />
+            </NavLink>
+            <NavLink to="/expenses">
+                <img src={Expenses()} alt="Expenses" />
+            </NavLink>
+            <NavLink to="/charts">
+                <img src={Reports()} alt="Reports" />
+            </NavLink>
+            <NavLink to="/settings">
+                <img src={Settings()} alt="Settings" />
+            </NavLink>
+        </Navbar>
+    );
 }
