e3b1c3fbc7b7b4427c5e37668edf810a5087d6f1
Roi-Livne <roilivne14@gmail.com>
2021-01-26 19:54:05 +0200

    hotfix for Footer

--- spender-frontend/src/components/footer.jsx
+++ spender-frontend/src/components/footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import styles from "../styles/footer.module.css";
+import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
-  return (
-    <div className={`${styles.footer} ${styles.buttonBottom}`}>Spender</div>
-  );
+    return (
+        <div className={`${styles.footer} ${styles.buttonBottom}`}>Spender</div>
+    );
 }
