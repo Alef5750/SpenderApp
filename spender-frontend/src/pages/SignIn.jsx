b4a1b35710fae5c0409daee0f74a95291dbf5a6a
Roi-Livne <roilivne14@gmail.com>
2021-01-27 11:22:47 +0200

    hotfix for google signin

--- spender-frontend/src/pages/SignIn.jsx
+++ spender-frontend/src/pages/SignIn.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Button } from "react-bootstrap";
 import GoogleButton from "react-google-button";
 
 import Footer from "../components/footer";
@@ -8,29 +7,24 @@ import spenderLogo from "../images/spenderLogo.png";
 import styles from "../styles/Navbar.module.css";
 
 export default function SignIn() {
-  const initGoogle = () => {
-    window.open(`http://localhost:5000/auth/google`, "_self");
-  };
+    const initGoogle = () => {
+        window.open(`http://localhost:5000/auth/google`, "_self");
+    };
 
-  return (
-    <div>
-      <div
-        className={` ${styles.navBar} ${styles.welcome} `}
-        sticky="top"
-        expand="sm"
-      >
-        Welcome to
-      </div>
-      <div className=" d-flex flex-column align-items-center">
-        {/* <div className="mx-5"> */}
-        <img className="my-4" src={spenderLogo} alt="Logo" />
-        {/* <Button className="my-5 py-3 col" onClick={() => initGoogle()}>
-              Sign in using Google
-            </Button> */}
-        <GoogleButton className="my-4" onClick={() => initGoogle()} />
-        {/* </div> */}
-        <Footer />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div
+                className={` ${styles.navBar} ${styles.welcome} `}
+                sticky="top"
+                expand="sm"
+            >
+                Welcome to
+            </div>
+            <div className=" d-flex flex-column align-items-center">
+                <img className="my-4" src={spenderLogo} alt="Logo" />
+                <GoogleButton className="my-4" onClick={() => initGoogle()} />
+                <Footer />
+            </div>
+        </div>
+    );
 }
