eba75d43408fd7d49fc8326ed1a8d890b39ee876
alef <alewitt@gmail.com>
2021-01-27 12:09:43 +0200

    readying AddCategories for api communication

--- spender-frontend/src/pages/AddCategory.jsx
+++ spender-frontend/src/pages/AddCategory.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Redirect, useHistory } from "react-router-dom";
+import { UserContext } from "../components/PrivateRoute";
 
 import { Formik } from "formik";
 import * as Yup from "yup";
@@ -15,94 +16,94 @@ import expenses_entertainement from "../images/expenses_entertainement.png";
 import expenses_other from "../images/expenses_other.png";
 //validation
 const formSchema = Yup.object().shape({
-    category: Yup.string().required("Please enter a category name").max(20),
+  category: Yup.string().required("Please enter a category name").max(20),
 });
 
 let arrayOfCards = [
-    {
-        title: "Monthly",
-        logo: expenses_monthly,
-        id: Math.random(),
-    },
-    { title: "Food", logo: expenses_food, id: Math.random() },
-    {
-        title: "Entertainment",
-        logo: expenses_entertainement,
-        id: Math.random(),
-    },
-    { title: "Other", logo: expenses_other, id: Math.random() },
+  {
+    title: "Monthly",
+    logo: expenses_monthly,
+    id: Math.random(),
+  },
+  { title: "Food", logo: expenses_food, id: Math.random() },
+  {
+    title: "Entertainment",
+    logo: expenses_entertainement,
+    id: Math.random(),
+  },
+  { title: "Other", logo: expenses_other, id: Math.random() },
 ];
 export default function AddCategory() {
-    const history = useHistory();
-    const [redirect, setDirect] = useState(null);
+  const user = useContext(UserContext);
+  const history = useHistory();
+  const [redirect, setDirect] = useState(null);
 
-    function handleNewCategory(newCategory) {
-        arrayOfCards.push({
-            title: newCategory.category,
-            logo: expenses_other,
-            id: Math.random(),
-        });
-        setDirect("/expenses");
-    }
-    if (redirect) {
-        return <Redirect to={redirect} />;
-    }
-    return (
-        <Formik
-            initialValues={{
-                category: "",
-            }}
-            validationSchema={formSchema}
-            onSubmit={(newCategory) => handleNewCategory(newCategory)}
-        >
-            {({
-                handleSubmit,
-                handleChange,
-                handleBlur,
-                values,
-                errors,
-                touched,
-            }) => {
-                return (
-                    <div className={styles.body}>
-                        <Navigation />
-                        <h1 className={`${styles.text} ${styles.h1}`}>
-                            Add a new category of your choice!
-                        </h1>
-                        <Form className={styles.form} onSubmit={handleSubmit}>
-                            <Form.Group>
-                                <input
-                                    className={styles.input}
-                                    type="text"
-                                    name={"category"}
-                                    value={values.category}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                />
-                            </Form.Group>
-                            {errors.category && touched.category && (
-                                <Alert variant="danger">
-                                    {errors.category}
-                                </Alert>
-                            )}
+  function handleNewCategory(newCategory) {
+    arrayOfCards.push({
+      title: newCategory.category,
+      logo: expenses_other,
+      id: Math.random(),
+    });
+    setDirect("/expenses");
+    console.log(user.categories);
+  }
+  if (redirect) {
+    return <Redirect to={redirect} />;
+  }
+  return (
+    <Formik
+      initialValues={{
+        category: "",
+      }}
+      validationSchema={formSchema}
+      onSubmit={(newCategory) => handleNewCategory(newCategory)}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        errors,
+        touched,
+      }) => {
+        return (
+          <div className={styles.body}>
+            <Navigation />
+            <h1 className={`${styles.text} ${styles.h1}`}>
+              Add a new category of your choice!
+            </h1>
+            <Form className={styles.form} onSubmit={handleSubmit}>
+              <Form.Group>
+                <input
+                  className={styles.input}
+                  type="text"
+                  name={"category"}
+                  value={values.category}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Form.Group>
+              {errors.category && touched.category && (
+                <Alert variant="danger">{errors.category}</Alert>
+              )}
 
-                            <Button className={styles.saveButton} type="submit">
-                                Save
-                            </Button>
-                        </Form>
-                        <Button
-                            className={`w-100 ${styles.buttonBottom}`}
-                            onClick={() => {
-                                history.push("/expenses");
-                            }}
-                        >
-                            Cancel
-                        </Button>
-                    </div>
-                );
-            }}
-        </Formik>
-    );
+              <Button className={styles.saveButton} type="submit">
+                Save
+              </Button>
+            </Form>
+            <Button
+              className={`w-100 ${styles.buttonBottom}`}
+              onClick={() => {
+                history.push("/expenses");
+              }}
+            >
+              Cancel
+            </Button>
+          </div>
+        );
+      }}
+    </Formik>
+  );
 }
 
 export { arrayOfCards };
--- spender-frontend/src/pages/SignIn.jsx
+++ spender-frontend/src/pages/SignIn.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Button } from "react-bootstrap";
 import GoogleButton from "react-google-button";
 
 import Footer from "../components/footer";
@@ -22,13 +21,8 @@ export default function SignIn() {
         Welcome to
       </div>
       <div className=" d-flex flex-column align-items-center">
-        {/* <div className="mx-5"> */}
         <img className="my-4" src={spenderLogo} alt="Logo" />
-        {/* <Button className="my-5 py-3 col" onClick={() => initGoogle()}>
-              Sign in using Google
-            </Button> */}
         <GoogleButton className="my-4" onClick={() => initGoogle()} />
-        {/* </div> */}
         <Footer />
       </div>
     </div>
